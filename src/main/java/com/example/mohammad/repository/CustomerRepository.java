24cf6c4f72fb7bbdd61f4052636203bba569e0fa
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-17 20:05:29 +0330

    create Customer  model,repo,service

---
+++ src/main/java/com/example/mohammad/repository/CustomerRepository.java
@@ -0,0 +1,19 @@
+package com.example.mohammad.repository;
+
+
+import com.example.mohammad.model.Customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    Optional<Customer> findByUsername(String username);
+
+    @Modifying
+    @Query("UPDATE Customer c SET c.password = :password WHERE c.username = :username")
+    void updatePassword(String password,String username);
+}
---
+++ src/main/java/com/example/mohammad/service/CustomerService.java
@@ -0,0 +1,79 @@
+package com.example.mohammad.service;
+
+import com.example.mohammad.exception.DuplicateInformationException;
+import com.example.mohammad.exception.InvalidEntityException;
+import com.example.mohammad.exception.NotFoundException;
+import com.example.mohammad.exception.NotValidPasswordException;
+import com.example.mohammad.model.Customer;
+import com.example.mohammad.repository.CustomerRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    private final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = validatorFactory.getValidator();
+
+
+    public boolean validate(Customer entity) {
+
+        Set<ConstraintViolation<Customer>> violations = validator.validate(entity);
+        if (violations.isEmpty())
+            return true;
+        else {
+            System.out.println("Invalid user data found:");
+            for (ConstraintViolation<Customer> violation : violations) {
+                System.out.println(violation.getMessage());
+            }
+            return false;
+        }
+    }
+
+    public Customer findById(Long id) {
+        return customerRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("the entity with %s not found", id)));
+    }
+
+    public Customer findByUsername(String username) {
+        return customerRepository.findByUsername(username).orElseThrow(() -> new NotFoundException(String.format("the entity with %s not found", username)));
+    }
+
+    public Customer registerCustomer(Customer customer) {
+        if (customerRepository.findByUsername(customer.getUsername()).isPresent())
+            throw new DuplicateInformationException(String.format("the customer with %s is duplicate", customer.getUsername()));
+        else if (!validate(customer)) {
+            throw new InvalidEntityException(String.format("the customer with %s have invalid variable", customer.getUsername()));
+        } else return customerRepository.save(customer);
+    }
+
+    public void deleteById(Long id) {
+        customerRepository.delete(findById(id));
+    }
+
+    public void deleteByUsername(String username) {
+        customerRepository.delete(findByUsername(username));
+    }
+
+    @Transactional
+    public void updatePassword(String username, String oldPassword, String newPassword, String finalNewPassword) {
+        String passRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+        if (customerRepository.findByUsername(username).isEmpty()) {
+            throw new NotFoundException(String.format("the entity with %s not found", username));
+        } else if (!newPassword.matches(passRegex) || !oldPassword.matches(passRegex) || !finalNewPassword.matches(passRegex)) {
+            throw new NotValidPasswordException(String.format("this password not valid"));
+        } else if (findByUsername(username).getPassword() != oldPassword || newPassword != finalNewPassword) {
+            throw new NotValidPasswordException(String.format("this password not valid"));
+        } else {
+            customerRepository.updatePassword(finalNewPassword, username);
+        }
+    }
+}
