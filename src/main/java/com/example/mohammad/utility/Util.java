c61e96e7f0b0e4f4ceb2e06cca7ecb7d8a7500db
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-19 23:57:19 +0330

    save image method

--- src/main/java/com/example/mohammad/utility/Util.java
+++ src/main/java/com/example/mohammad/utility/Util.java
@@ -4,10 +4,12 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import org.springframework.stereotype.Component;
 
+import java.io.*;
 import java.util.Objects;
 import java.util.Set;
-
+@Component
 public class Util {
     ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
     Validator validator = validatorFactory.getValidator();
@@ -25,5 +27,49 @@ public class Util {
             }
             return false;
         }
+    }
+
+    public static byte[] saveImage(String path) {
+        File file1 = new File(path);
+        if (isJPEG(new File(path)) && file1.length() < 300000) {
+            File file = new File(path);
+            InputStream is = null;
+            try {
+                is = new FileInputStream(file);
+            } catch (FileNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+            byte[] bytes = new byte[(int) file.length()];
+            try {
+                is.read(bytes);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            return bytes;
+        }
+
+        return null;
+    }
+
+    private static Boolean isJPEG(File filename) {
+        boolean flag = false;
+        DataInputStream ins;
+        try {
+            ins = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            flag = ins.readInt() == 0xffd8ffe0;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                ins.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return flag;
     }
 }
