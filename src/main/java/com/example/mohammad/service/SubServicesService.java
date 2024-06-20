c72a8568f713c619d5ee9ceff90b28c82ed62558
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-20 12:57:39 +0330

    add serviceTest for SubServicesService

--- src/main/java/com/example/mohammad/service/SubServicesService.java
+++ src/main/java/com/example/mohammad/service/SubServicesService.java
@@ -3,8 +3,6 @@ package com.example.mohammad.service;
 import com.example.mohammad.exception.DuplicateInformationException;
 import com.example.mohammad.exception.InvalidEntityException;
 import com.example.mohammad.exception.NotFoundException;
-import com.example.mohammad.exception.NullListException;
-import com.example.mohammad.model.Services;
 import com.example.mohammad.model.SubServices;
 import com.example.mohammad.repository.SubServicesRepository;
 import jakarta.transaction.Transactional;
@@ -21,13 +19,13 @@ import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
-public class SubServicesService  {
+public class SubServicesService {
     private final SubServicesRepository subServicesRepository;
     private final ServicesService servicesService;
-  ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-     Validator validator = validatorFactory.getValidator();
+    ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    Validator validator = validatorFactory.getValidator();
 
-//    private final Validator validator ;
+
     public boolean validate(SubServices entity) {
 
         Set<ConstraintViolation<SubServices>> violations = validator.validate(entity);
@@ -36,49 +34,42 @@ public class SubServicesService  {
         else {
             System.out.println("Invalid user data found:");
             for (ConstraintViolation<SubServices> violation : violations) {
-                System.out.println(violation.getMessage());
-            }
-            return false;
-        }
+                System.out.println(violation.getMessage());}
+            return false;}
     }
-    public List<SubServices> findAllSubServiceOfService(Services services){
-        if (subServicesRepository.findAllByServices(services)==null)
-            throw new NullListException(String.format("not found any sub service for %s service ",services.getServiceName()));
-        return subServicesRepository.findAllByServices(services);
+
+    public List<SubServices> findAllSubServiceOfService(String serviceName) {
+       return subServicesRepository.findAllByServices(servicesService.findByNameServices(serviceName));
     }
-    public SubServices saveSubService(SubServices subServices , String serviceName){
+
+    public SubServices saveSubService(SubServices subServices, String serviceName) {
         subServices.setServices(servicesService.findByNameServices(serviceName));
         if (!validate(subServices))
             throw new InvalidEntityException(String.format("the customer with %s have invalid variable", subServices.getName()));
-        else if (subServicesRepository.findByName(subServices.getName()).isPresent())
-        {throw new DuplicateInformationException(String.format("the sub service with %s is duplicate", subServices.getName()));}
-        return subServicesRepository.save(subServices);
-    }
+        else if (subServicesRepository.findByName(subServices.getName()).isPresent()) {
+            throw new DuplicateInformationException(String.format("the sub service with %s is duplicate", subServices.getName()));}
+        return subServicesRepository.save(subServices);}
     public SubServices findById(Long id) {
         return subServicesRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("the entity with %s not found", id)));
     }
-
     public SubServices findBySubServiceName(String name) {
         return subServicesRepository.findByName(name).orElseThrow(() -> new NotFoundException(String.format("the entity with %s not found", name)));
     }
-    public void deleteById(Long id) {
-        subServicesRepository.delete(findById(id));
-    }
-
-    public void deleteByUsername(String name) {
+    public void deleteByName(String name) {
         subServicesRepository.delete(findBySubServiceName(name));
     }
-@Transactional
-    public void updateDescription(String description,String name){
+    @Transactional
+    public void updateDescription(String description, String name) {
         if (subServicesRepository.findByName(name).isEmpty())
-            throw new NullListException(String.format("not found any sub service with %s  ",name));
-       else subServicesRepository.updateDescription(description,name);
+            throw new NotFoundException(String.format("not found any sub service with %s  ", name));
+        else subServicesRepository.updateDescription(description, name);
     }
     @Transactional
-    public void updateBasePrice(double basePrice ,String name){
+    public void updateBasePrice(double basePrice, String name) {
         if (subServicesRepository.findByName(name).isEmpty())
-            throw new NullListException(String.format("not found any sub service with %s  ",name));
-        else subServicesRepository.updateBasePrice(basePrice,name);
+            throw new NotFoundException(String.format("not found any sub service with %s  ", name));
+        else if (basePrice<0){
+            throw new InvalidEntityException(String.format("the customer with %s have invalid price", name));}
+        else subServicesRepository.updateBasePrice(basePrice, name);
     }
-
 }
--- src/test/java/com/example/mohammad/service/SubServicesServiceTest.java
+++ src/test/java/com/example/mohammad/service/SubServicesServiceTest.java
@@ -1,4 +1,126 @@
 package com.example.mohammad.service;
 
+import com.example.mohammad.exception.DuplicateInformationException;
+import com.example.mohammad.exception.InvalidEntityException;
+import com.example.mohammad.exception.NotFoundException;
+import com.example.mohammad.model.Customer;
+import com.example.mohammad.model.Services;
+import com.example.mohammad.model.SubServices;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SubServicesServiceTest {
+
+    @Autowired
+    private SubServicesService subServicesService;
+
+    public static SubServices currentSubServices;
+    public static SubServices duplicateSubServices;
+    public static SubServices notFoundSubServices;
+    public static SubServices notValidSubServices;
+
+    @BeforeAll
+    public static void createCustomer(){
+        currentSubServices=SubServices.builder()
+                .name("joloBandi").basePrice(100).description("jolo bandi mashin")
+                .build();
+        duplicateSubServices=SubServices.builder().name("joloBandi").basePrice(100).description("jolo bandi mashin")
+                .build();
+        notFoundSubServices=SubServices.builder().name("notfound").basePrice(0).description("xxx")
+                .build();
+        notValidSubServices=SubServices.builder().name("notValid").basePrice(-10).description("xxx")
+                .build();
+    }
+    @Test
+    @Order(1)
+    void ThrowsNotFoundExceptionFindByUsernameTest() {
+        assertThrows(NotFoundException.class, () -> subServicesService.findBySubServiceName("nonexistentUser"));
+    }
+    @Test
+    @Order(2)
+    void ThrowsNotFoundExceptionFindByIdTest() {
+        assertThrows(NotFoundException.class, () -> subServicesService.findById(20800L));
+    }
+
+    @Test
+    @Order(3)
+    void saveCorrect() {
+        SubServices subServices=subServicesService.saveSubService(currentSubServices,"vehicle");
+        assertEquals(subServices.getName(),subServicesService.findBySubServiceName("joloBandi").getName());
+    }
+    @Test
+    @Order(4)
+    void invalidEntityExceptionSaveCorrect() {
+        assertThrows(InvalidEntityException.class, () -> subServicesService.saveSubService(notValidSubServices,"vehicle"));
+    }
+
+    @Test
+    @Order(5)
+    void duplicateInformationExceptionSaveCorrect() {
+        assertThrows(DuplicateInformationException.class, () -> subServicesService.saveSubService(duplicateSubServices,"vehicle"));
+    }
+    @Test
+    @Order(6)
+    void findAllSubServiceOfServiceTest(){
+        List<SubServices> vehicle = subServicesService.findAllSubServiceOfService("vehicle");
+        assertTrue(vehicle.size()>2);
+    }
+
+    @Test
+    @Order(7)
+    void findByIdTest() {
+        SubServices subServices=subServicesService.findById(4l);
+        assertEquals("carWash", subServices.getName());
+    }
+
+    @Test
+    @Order(8)
+    void findByNameTest() {
+        SubServices subServices=subServicesService.findBySubServiceName("carWash");
+        assertEquals("carWash", subServices.getName());
+    }
+
+    @Test
+    @Order(9)
+    void notFoundExceptionUpdateDescriptionTest(){
+        assertThrows(NotFoundException.class, () -> subServicesService.updateDescription("xxx","notFound"));
+    }
+
+    @Test
+    @Order(10)
+    void updateDescriptionTest(){
+        subServicesService.updateDescription("updated description","joloBandi");
+        assertEquals("updated description", subServicesService.findBySubServiceName("joloBandi").getDescription());}
+    @Test
+    @Order(11)
+    void notFoundExceptionUpdateBasePriceTest(){
+        assertThrows(NotFoundException.class, () -> subServicesService.updateBasePrice(50,"notFound"));
+    }
+    @Test
+    @Order(12)
+    void invalidEntityExceptionUpdateBasePriceTest(){
+        assertThrows(InvalidEntityException.class, () -> subServicesService.updateBasePrice(-50,"joloBandi"));
+    }
+
+    @Test
+    @Order(13)
+    void updateBasePriceTest(){
+        subServicesService.updateBasePrice(Double.parseDouble("509"),"joloBandi");
+        assertEquals(509, subServicesService.findBySubServiceName("joloBandi").getBasePrice());}
+
+    @Test
+    @Order(14)
+    void deleteByNameTest(){
+
+        subServicesService.deleteByName("carWash");
+//        assertThrows(NotFoundException.class, () -> subServicesService.findBySubServiceName("joloBandi"));
+    }
 }
