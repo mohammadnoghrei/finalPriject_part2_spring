66de35231b2a83f51b9e2ffb1116faaf2f5b9dc2
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-20 21:40:43 +0330

    cleaning the code

---
+++ src/main/java/com/example/mohammad/config/SpringCustomConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.mohammad.config;
+
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCustomConfiguration {
+
+//    @Bean
+//    public ValidatorFactory validatorFactory () {return Validation.buildDefaultValidatorFactory();}
+//    @Bean
+//    public Validator validator (){return validatorFactory().getValidator();};
+}
